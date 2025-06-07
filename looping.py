29827668541396b42a63ae195e36b8b0866dd157
Nathan Dias Pereira <106447536+NDWHEY@users.noreply.github.com>
2025-06-07 00:54:59 +0000

    looping

---
+++ looping.py
@@ -0,0 +1,32 @@
+# nomes = ["Ana", "Nathan", "Rezende"]
+
+# idades = [25, 26, 22]
+
+# trabalho = ["Tatuadora", "burracheiro", "Programador"]
+
+# salarios = [5000, 4000, 10000]
+
+# for nomes, idades, trabalho, salarios,  in zip(nomes, idades, trabalho, salarios):
+#     print(f"{nomes}\
+#            tem {idades} anos\
+#               trabalha como {trabalho}\
+#               ganha {salarios} por mês")
+
+# dados = {1:"nome", 2:"Ana", 3:"idade"}
+# for chave in dados:
+#     print(chave)
+
+# for valor in dados.values():
+#     print(valor)
+
+# for chave, valor in dados.items():
+#     print(f"{chave} + {valor} :)")
+
+# lista = [0,1,4,9,16]
+
+# dicionario = {1: "a", 2:"b", 3:"4"}
+
+# print(type(dicionario))
+
+# quadrados = [(r,a) for r,a in dicionario.items()]
+# print(quadrados)
