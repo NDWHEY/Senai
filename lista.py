f94c066264f78e18d81565efcebfea066bf40dae
Nathan Dias Pereira <106447536+NDWHEY@users.noreply.github.com>
2025-06-11 00:58:36 +0000

    ex.10/06

---
+++ lista.py
@@ -0,0 +1,26 @@
+# 1
+
+
+# for num  in range(0, 201,11):
+#     if num % 2== 0:
+#         print(num)
+
+#2
+
+# animais = ["gato", "cachorro", "pássaro", "tigre"]
+
+# i = "Aminal:"
+
+# for animal in animais :
+#     print( i + animal)
+
+#3
+
+# lista = ["ana", "bruno", "carlos"]
+
+# for nomes in enumerate(lista):
+#     print(nomes) 
+
+ 
+# for nomes in enumerate(reversed(lista)):
+#     print(nomes)
