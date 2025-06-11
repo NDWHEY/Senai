b44b62addd9fdbbd68b72085a9ba814567c7ba96
Nathan Dias Pereira <106447536+NDWHEY@users.noreply.github.com>
2025-06-11 23:32:09 +0000

    lista.py

--- lista.py
+++ lista.py
@@ -24,3 +24,16 @@
  
 # for nomes in enumerate(reversed(lista)):
 #     print(nomes)
+
+#4
+# produtos = ["Arroz", "Feijão", "Café", "Leite"]
+
+# precos = [5.20,4.50,4.57,5.3]
+
+# for pp in zip(produtos, precos):
+#     print(pp)
+
+#5
+# frase = "Python é a melhor linguagem do mundo"
+# for c in frase:
+#     print(c, end='  ')
