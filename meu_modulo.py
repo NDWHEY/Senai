a3c0c05b07be605da36a7437b2d72b042c310c28
Nathan Dias Pereira <106447536+NDWHEY@users.noreply.github.com>
2025-06-18 00:55:38 +0000

    atividade12

---
+++ meu_modulo.py
@@ -0,0 +1,25 @@
+import random
+
+# def atividade1():
+#     al = random.randint(5,10)
+#     return al
+
+# def atividade2():
+#     al1 = random.randint(1,10)
+#     al2 = random.randint(2,10)
+#     al3 = random.randint(3,10)
+#     print(al1, al2, al3)
+
+# def atividade3():
+#     al  = random.randrange(10,30)
+#     print(al)
+
+# def atividade4():
+#     for i in range(10, 0, -1):
+#         print(i)
+#     print("Fogo")    
+
+num1 = int(input("Digite um número inteiro posistivo "))
+
+for num1 in range(0, num1+1):
+    print(num1)
